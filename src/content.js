d06c8c0ac08a203ec017f5c7e430e309f77e25c0
PIGC <PigCart@outlook.com>
2023-10-11 00:54:58 +0100

    implement grader (& adjust prompt detection)

--- src/content.js
+++ src/content.js
@@ -4,8 +4,6 @@ var previousURL;
 var translations;
 var foundPrompt;
 var liveSession;
-var oldSession;
-var compactedGrader = [];
 
 // Insert web accessible script to override fetch method
 var fetchScript = document.createElement('script');
@@ -14,13 +12,9 @@ document.head.appendChild(fetchScript);
 
 // Listen for events from the fetch override
 document.addEventListener('fetchSessions', (event) => {
-    oldSession = liveSession;
     liveSession = event.detail;
     console.log('intercepted session fetch:', liveSession);
     // on learn page sometimes a session is sent. it is added to prefetched sessions.
-    if (liveSession.id == oldSession.id) {alert("session Id matches old session")} // do sessions ever get sent twice?
-    //TODO: would be nice to identify and track the correct session
-    // did a practice session that only seemed to use the 'challenges' array. when are the other challenge types used? they seems to be duplicates... mostly?
 });
 
 // cancel enter key presses to prevent the challenge ending and instead run the custom answer checker
@@ -30,7 +24,6 @@ window.addEventListener("keydown", function(keyboardEvent) {
         keyboardEvent.preventDefault();
         const customNextButton = this.document.getElementById("yourButtonButBetter");
         if (customNextButton != null) {
-            console.log("enter key caught");
             checkAnswer();
         }
     }
@@ -55,17 +48,15 @@ document.arrive('button[data-test="player-next"]', {fireOnAttributesModification
     nextButton = document.querySelector('button[data-test="player-next"]');
     if (nextButton != null) {
         if (document.getElementById("yourButtonButBetter") == null) {
-            console.log("Inserting custom next button");
             const newNode = document.createElement("div");
             //TODO: actual stylesheets for additional elements 
             newNode.style.height = "100%"; newNode.style.width = "100%"; newNode.style.position = "absolute"; newNode.style.top = "0"; newNode.style.cursor = "pointer";
             newNode.id = "yourButtonButBetter";
             newNode.onclick = function() {checkAnswer()};
             nextButton.parentElement.appendChild(newNode);
-            console.log(newNode);
         }
     } else {
-        console.log("Couldn't find nextButton");
+        console.error("Couldn't find nextButton");
     }
 });
 // Check answer by comparing the textarea content to the list of translations
@@ -79,24 +70,23 @@ function checkAnswer() {
             if (translations.length > 0) {
                 const userinput = document.querySelector('textarea[data-test="challenge-translate-input"]').textContent.toLowerCase();
                 for (var i = 0; i < translations.length; i++) {
-                    console.log(translations[i]);
                     if (translations[i].test(userinput)) {
                         console.log("correct answer, clicking nextButton: " + userinput);
                         nextButton.click()
+                        return
                     } else {
-                        console.log("No match for: " + userinput);
+                        console.error("No match for: " + userinput);
+                        return
                     }
                 }
             } else {
-                console.log("find 0 translations, clicking nextButton");
+                console.error("Could not find translations");
                 nextButton.click();
             }
         } else {
-            console.log("could not find textarea, clicking nextButton");
             nextButton.click();
         }
     } else {
-        console.log("Not a translation challenge, clicking nextButton");
         nextButton.click();
     }
 }
@@ -110,7 +100,6 @@ document.arrive('div[data-test="challenge challenge-completeReverseTranslation"]
 
 // Automatically click button to switch from bubbles to keyboard and fetch the translations for the displayed prompt
 function onTranslationChallenge() {
-    console.log('New translation exercise!')
     const difficultyButton = document.querySelector('button[data-test="player-toggle-keyboard"]');
     if (difficultyButton != null) {
         if (difficultyButton.textContent == "Make harder" || difficultyButton.textContent == "Use keyboard") {
@@ -124,10 +113,11 @@ function onTranslationChallenge() {
     setTimeout(function() {
         // get prompt from displayed sentence tokens
         const sentenceTokenNodes = document.querySelectorAll('[data-test="hint-token"]');
-        var sentence = "";
+        let labels = [];
         for (var i = 0; i < sentenceTokenNodes.length; i++) {
-            sentence = sentence.concat(sentenceTokenNodes[i].textContent);
+            labels.push(sentenceTokenNodes[i].getAttribute('aria-label'));
         }
+        let sentence = labels.join(" ").toLowerCase()
         //TODO: Support audio prompts (grab ids for audio clips? i think the clip urls are stored in the challenges?)
         getPromptTranslations(sentence);
     }, 2000);
@@ -139,32 +129,29 @@ function loadPrefetchedSessions() {
     } else {
         let openRequest = window.indexedDB.open('duolingo');
         openRequest.onsuccess = function() {
-            console.log("opened duolingo database");
             let IDBRequest = openRequest.result.transaction('prefetchedSessions').objectStore('prefetchedSessions').getAll();
             IDBRequest.onsuccess = function() {
-                console.log("Loaded Prefetched Sessions");
                 prefetchedSessions = IDBRequest.result;
             }
             IDBRequest.onerror = function(event) {
-                console.log(event);
+                console.error(event);
             }
         };
         openRequest.onerror = function(event) {
-            console.log(event);
+            console.error(event);
         }
     }
 }
 // go through all challenges from the live and prefetched sessions to search for translations of a prompt
 function getPromptTranslations(prompt) {
-    //FIXME: somethings fucked! D:
     translations = [];
     foundPrompt = false;
-    //if (liveSession != null) { searchSessionChallenges(liveSession, prompt) }
+    if (liveSession != null) { searchSessionChallenges(liveSession, prompt) }
     prefetchedSessions.forEach((prefetchedSession) => {
         searchSessionChallenges(prefetchedSession.session, prompt)
     });
     if (foundPrompt == false) {
-        console.error("Couldn't find challenge with prompt: " + prompt);
+        console.error("Couldn't find challenge with prompt: ", prompt);
     }
 };
 
@@ -185,56 +172,86 @@ function searchSessionChallenges(session, prompt) {
         extractTranslationsFromChallenge(challenge, prompt);
     });
 }
-// get compactTranslations and turn them into regex
+// get translations from a challenge and turn them into regex. Fall back to grader if compactTranslations arent present
 function extractTranslationsFromChallenge(challenge, prompt) {
-    if (challenge.prompt == prompt) {
-        foundPrompt = true;
-        if (Object.hasOwn(challenge, "compactTranslations")) {
-            challenge.compactTranslations.forEach((compactTranslation) => {
-                translations.push(new RegExp(compactTranslation.toLowerCase()
-                        .replaceAll("[", "(")
-                        .replaceAll("]", ")")
-                        .replaceAll("/", "|")
-                        .replaceAll("?", "\\??")
-                        .replaceAll(".", "\\.?")
-                        .replaceAll("!", "!?")
-                        .replaceAll(",", ",?")
-                        .replaceAll(" ", " ?")
-                ));
-            });
-        }
-        compactedGrader = [];
-        consumeVertex(challenge.grader.vertices, 1, "");
-        console.log(compactedGrader);
-        //console.log(compactedGrader);
-        //console.log(challenge.compactTranslations);
-        //console.log(challenge.grader.vertices);
-        return
-        if (translations.length == 0) {
-            console.error("Unable to extract translations from challenge:", challenge.prompt);
-            //TODO: compactTranslations don't seem very reliable. This should be rewritten to use the grader object, which seems to be always present and have all translations.
-        } else {
-            console.log("translations:", translations);
+    if (challenge.prompt != null){
+        let challengePromptRegex = sentenceToRegex(challenge.prompt);
+        if (challengePromptRegex.test(prompt)) {
+            foundPrompt = true;
+            if (Object.hasOwn(challenge, "compactTranslations")) {
+                challenge.compactTranslations.forEach((compactTranslation) => {
+                    translations.push(sentenceToRegex(compactTranslation));
+                });
+            }
+            readVertex(challenge.grader.vertices, 0, "");
+            if (translations.length == 0) {
+                console.error("Unable to extract translations from challenge:", challenge.prompt);
+            } else {
+                console.log("translations:", translations);
+            }
         }
     }
 }
 
-//good debug sentence, 132 solution variants (!!!)
-setTimeout(() => {
-    getPromptTranslations("The tiger thinks that you look like a good person.");
-}, 2000);
-
-// recursively traverse the solution graph to resolve each translation
-function consumeVertex(vertices, index, compactedTranslation) {
+// recursively traverse the solution graph to resolve each translation, discarding typo entries
+function readVertex(vertices, index, graderTranslation) {
     let vertex = vertices[index];
     if (vertex.length == 0) {
         // the grader ends with an empty vertex, so the translation should be complete at this point.
-        compactedGrader.push(compactedTranslation);
+        translations.push(sentenceToRegex(graderTranslation));
     } else {
-        vertex.forEach((token) => {
-            consumeVertex(vertices, token.to, compactedTranslation + token.lenient);
-            // it works!!!
-            //TODO: including token.orig should make this on par with compact translations
-        })
+        // bundle tokens in this vertex together if they have the same destination
+        // not really neccessary but make it easier to read when debugging
+        let shouldBundleTokens = false;
+        if (vertex.length > 1) {
+            let tokensWithSameDestination = 0;
+            vertex.forEach((token) => {
+                if (token.to == vertex[0].to) {
+                    tokensWithSameDestination++
+                }
+            })
+            shouldBundleTokens = tokensWithSameDestination == vertex.length
+            if (shouldBundleTokens) {
+                let bundledTokens = []
+                vertex.forEach((token) => {
+                    if (token.type != "typo" && token.auto != true) {
+                        if (Object.hasOwn(token, "orig")) {
+                            bundledTokens.push(token.orig);
+                        } else {
+                            bundledTokens.push(token.lenient);
+                        }
+                    }
+                })
+                readVertex(vertices, vertex[0].to, graderTranslation + "[" + bundledTokens.join("/") + "]");
+                return
+            }
+        }
+        // or continue building translation without bundling, branching for each token
+        if (!shouldBundleTokens) {
+            vertex.forEach((token) => {
+                if (token.type != "typo" && token.auto != true) {
+                    if (Object.hasOwn(token, "orig")) {
+                        readVertex(vertices, token.to, graderTranslation + token.orig);
+                    } else {
+                        readVertex(vertices, token.to, graderTranslation + token.lenient);
+                    }
+                }
+            })
+        }
     }
+}
+
+// convert compactTranslations to valid regex and ignore punctuation in translations or prompts
+function sentenceToRegex(translationString) {
+    return RegExp("^" + translationString.toLowerCase()
+        .replaceAll("[", "(")
+        .replaceAll("]", ")")
+        .replaceAll("/", "|")
+        .replaceAll("?", "\\??")
+        .replaceAll(".", "\\.?")
+        .replaceAll("!", "!?")
+        .replaceAll(",", ",?")
+        .replaceAll(" ", " ?")
+        + "$"
+    )
 }
