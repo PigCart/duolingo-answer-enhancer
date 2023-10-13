05f3117841b90d1de6b442a0735846b7f91f40df
PIGC <PigCart@outlook.com>
2023-10-13 23:54:04 +0100

    slight rewrite & basic hint GUI

--- src/content.js
+++ src/content.js
@@ -1,9 +1,11 @@
 var nextButton;
 var prefetchedSessions;
 var previousURL;
-var translations;
+var solutionGrader;
 var foundPrompt;
 var liveSession;
+var translationCorrect;
+var hintMessage;
 
 // Insert web accessible script to override fetch method
 var fetchScript = document.createElement('script');
@@ -43,13 +45,11 @@ setInterval(() => {
         yourButtonButBetter.onclick = function() {checkAnswer()}
     }
 }, 1000)
-// insert custom next button on top of the original
-document.arrive('button[data-test="player-next"]', {fireOnAttributesModification: true, existing: true}, () => {
-    nextButton = document.querySelector('button[data-test="player-next"]');
+
+function replaceNextButton() {
     if (nextButton != null) {
         if (document.getElementById("yourButtonButBetter") == null) {
             const newNode = document.createElement("div");
-            //TODO: actual stylesheets for additional elements 
             newNode.style.height = "100%"; newNode.style.width = "100%"; newNode.style.position = "absolute"; newNode.style.top = "0"; newNode.style.cursor = "pointer";
             newNode.id = "yourButtonButBetter";
             newNode.onclick = function() {checkAnswer()};
@@ -58,7 +58,54 @@ document.arrive('button[data-test="player-next"]', {fireOnAttributesModification
     } else {
         console.error("Couldn't find nextButton");
     }
+}
+
+function tryAgainPrompt(hintMessage) {
+    if (document.getElementById("answer-enhancer-retry-prompt") == null) {
+        console.log("try again lol :3");
+        const retryElement = document.createElement("div");
+        retryElement.id = "answer-enhancer-retry-prompt";
+        retryElement.style.background = "#ffeebb88"; retryElement.style.width = "100%"; retryElement.style.zIndex = "24"; retryElement.style.position = "relative";
+        retryElement.style.bottom = "50px"; retryElement.style.padding = "16px"; retryElement.style.maxWidth = "1080px"; retryElement.style.margin = "0 auto";
+        const retryHeader = document.createElement("h2");
+        retryHeader.style.margin = "0"; retryHeader.textContent = "Try again";
+        const retryHint = document.createElement("div");
+        retryHint.id = "answer-enhancer-retry-hint";
+        retryHint.textContent = "Hint: " + hintMessage;
+        document.getElementById("session/PlayerFooter").parentElement.appendChild(retryElement);
+        retryElement.appendChild(retryHeader);
+        retryElement.appendChild(retryHint);
+    } else {
+        document.getElementById("answer-enhancer-retry-hint").textContent = "Hint: " + hintMessage;
+    }
+}
+
+// remove retry prompt when footer updates
+document.arrive('div[data-test="blame blame-correct"]', {fireOnAttributesModification: true, existing: true}, () => {
+    if (document.getElementById("answer-enhancer-retry-prompt") != null) {
+        document.getElementById("answer-enhancer-retry-prompt").remove();
+    }
 });
+document.arrive('div[data-test="blame blame-incorrect"]', {fireOnAttributesModification: true, existing: true}, () => {
+    if (document.getElementById("answer-enhancer-retry-prompt") != null) {
+        document.getElementById("answer-enhancer-retry-prompt").remove();
+    }
+});
+
+// insert custom next button on top of the original
+document.arrive('button[data-test="player-next"]', {fireOnAttributesModification: true, existing: true}, () => {
+    nextButton = document.querySelector('button[data-test="player-next"]');
+    replaceNextButton();
+});
+
+// detect arrival of a new translation challenge
+document.arrive('div[data-test="challenge challenge-translate"]', {fireOnAttributesModification: true, existing: true}, () => {
+    onTranslationChallenge()
+});
+document.arrive('div[data-test="challenge challenge-completeReverseTranslation"]', {fireOnAttributesModification: true, existing: true}, () => {
+    onTranslationChallenge()
+});
+
 // Check answer by comparing the textarea content to the list of translations
 function checkAnswer() {
     if (nextButton == null) {
@@ -67,22 +114,20 @@ function checkAnswer() {
     }
     if (document.querySelector('div[data-test="challenge challenge-translate"]') != null || document.querySelector('div[data-test="challenge challenge-completeReverseTranslation"]') != null) {
         if (document.querySelector('textarea[data-test="challenge-translate-input"]') != null) {
-            if (translations.length > 0) {
-                const userinput = document.querySelector('textarea[data-test="challenge-translate-input"]').textContent.toLowerCase();
-                for (var i = 0; i < translations.length; i++) {
-                    if (translations[i].test(userinput)) {
-                        console.log("correct answer, clicking nextButton: " + userinput);
-                        nextButton.click()
-                        return
-                    } else {
-                        console.error("No match for: " + userinput);
-                        return
-                    }
+            //if (translations.length > 0) {
+                const userinput = document.querySelector('textarea[data-test="challenge-translate-input"]').textContent.toLowerCase().replaceAll(" ", "");
+                translationCorrect = false;
+                console.log(solutionGrader.vertices);
+                readVertex(1, userinput);
+                if (!translationCorrect) {
+                    tryAgainPrompt(hintMessage);
+                } else {
+                    nextButton.click();
                 }
-            } else {
+            /*} else {
                 console.error("Could not find translations");
                 nextButton.click();
-            }
+            }*/
         } else {
             nextButton.click();
         }
@@ -90,13 +135,6 @@ function checkAnswer() {
         nextButton.click();
     }
 }
-// detect arrival of a new translation challenge
-document.arrive('div[data-test="challenge challenge-translate"]', {fireOnAttributesModification: true, existing: true}, () => {
-    onTranslationChallenge()
-});
-document.arrive('div[data-test="challenge challenge-completeReverseTranslation"]', {fireOnAttributesModification: true, existing: true}, () => {
-    onTranslationChallenge()
-});
 
 // Automatically click button to switch from bubbles to keyboard and fetch the translations for the displayed prompt
 function onTranslationChallenge() {
@@ -111,22 +149,23 @@ function onTranslationChallenge() {
     }
     // delay to avoid picking up the previous challenge through transition animations
     setTimeout(function() {
-        // get prompt from displayed sentence tokens
+        // get prompt from sentence token labels
         const sentenceTokenNodes = document.querySelectorAll('[data-test="hint-token"]');
-        let labels = [];
-        for (var i = 0; i < sentenceTokenNodes.length; i++) {
-            labels.push(sentenceTokenNodes[i].getAttribute('aria-label'));
+        if (sentenceTokenNodes == null) {
+            console.error("Could not get sentence token nodes")
+        } else {
+            let sentence = "";
+            for (var i = 0; i < sentenceTokenNodes.length; i++) {
+                sentence = sentence.concat(sentenceTokenNodes[i].getAttribute('aria-label').toLowerCase());
+            }
+            //TODO: Support audio prompts (grab ids for audio clips? i think the clip urls are stored in the challenges?)
+            findGraderForPrompt(sentence);
         }
-        let sentence = labels.join(" ").toLowerCase()
-        //TODO: Support audio prompts (grab ids for audio clips? i think the clip urls are stored in the challenges?)
-        getPromptTranslations(sentence);
     }, 2000);
 }
 // open duolingo indexed database and request prefetched sessions
 function loadPrefetchedSessions() {
-    if (!('indexedDB' in window)) {
-        alert("This browser doesn't support IndexedDB");
-    } else {
+    if ('indexedDB' in window) {
         let openRequest = window.indexedDB.open('duolingo');
         openRequest.onsuccess = function() {
             let IDBRequest = openRequest.result.transaction('prefetchedSessions').objectStore('prefetchedSessions').getAll();
@@ -143,10 +182,12 @@ function loadPrefetchedSessions() {
     }
 }
 // go through all challenges from the live and prefetched sessions to search for translations of a prompt
-function getPromptTranslations(prompt) {
+function findGraderForPrompt(prompt) {
     translations = [];
     foundPrompt = false;
-    if (liveSession != null) { searchSessionChallenges(liveSession, prompt) }
+    if (liveSession != null) {
+        searchSessionChallenges(liveSession, prompt)
+    }
     prefetchedSessions.forEach((prefetchedSession) => {
         searchSessionChallenges(prefetchedSession.session, prompt)
     });
@@ -158,86 +199,62 @@ function getPromptTranslations(prompt) {
 // check translations for all challenge types for the specified session
 function searchSessionChallenges(session, prompt) {
     session.challenges.forEach((challenge) => {
-        extractTranslationsFromChallenge(challenge, prompt);
+        getChallengeGrader(challenge, prompt);
     });
     if (Object.hasOwn(session, "adaptiveChallenges")) {
         session.adaptiveChallenges.forEach((challenge) => {
-            extractTranslationsFromChallenge(challenge, prompt);
+            getChallengeGrader(challenge, prompt);
         });
     }
     session.adaptiveInterleavedChallenges.challenges.forEach((challenge) => {
-        extractTranslationsFromChallenge(challenge, prompt);
+        getChallengeGrader(challenge, prompt);
     });
     session.easierAdaptiveChallenges.forEach((challenge) => {
-        extractTranslationsFromChallenge(challenge, prompt);
+        getChallengeGrader(challenge, prompt);
     });
 }
-// get translations from a challenge and turn them into regex. Fall back to grader if compactTranslations arent present
-function extractTranslationsFromChallenge(challenge, prompt) {
+// get translations from a challenge and turn them into regex.
+function getChallengeGrader(challenge, prompt) {
     if (challenge.prompt != null){
         let challengePromptRegex = sentenceToRegex(challenge.prompt);
         if (challengePromptRegex.test(prompt)) {
             foundPrompt = true;
-            if (Object.hasOwn(challenge, "compactTranslations")) {
-                challenge.compactTranslations.forEach((compactTranslation) => {
-                    translations.push(sentenceToRegex(compactTranslation));
-                });
+            solutionGrader = challenge.grader
+            if (solutionGrader.version != 0) {
+                console.log("Grader has updated and may result in errors");
             }
-            readVertex(challenge.grader.vertices, 0, "");
-            if (translations.length == 0) {
-                console.error("Unable to extract translations from challenge:", challenge.prompt);
-            } else {
-                console.log("translations:", translations);
+            if (solutionGrader.whitespaceDelimited != true) {
+                console.log("this challenge is not whitespace delimited and may result in errors");
             }
         }
     }
 }
 
 // recursively traverse the solution graph to resolve each translation, discarding typo entries
-function readVertex(vertices, index, graderTranslation) {
-    let vertex = vertices[index];
+function readVertex(index, userinput) {
+    let vertex = solutionGrader.vertices[index];
     if (vertex.length == 0) {
         // the grader ends with an empty vertex, so the translation should be complete at this point.
-        translations.push(sentenceToRegex(graderTranslation));
+        translationCorrect = true;
+        console.log("done");
     } else {
-        // bundle tokens in this vertex together if they have the same destination
-        // not really neccessary but make it easier to read when debugging
-        let shouldBundleTokens = false;
-        if (vertex.length > 1) {
-            let tokensWithSameDestination = 0;
-            vertex.forEach((token) => {
-                if (token.to == vertex[0].to) {
-                    tokensWithSameDestination++
-                }
-            })
-            shouldBundleTokens = tokensWithSameDestination == vertex.length
-            if (shouldBundleTokens) {
-                let bundledTokens = []
-                vertex.forEach((token) => {
-                    if (token.type != "typo" && token.auto != true) {
-                        if (Object.hasOwn(token, "orig")) {
-                            bundledTokens.push(token.orig);
-                        } else {
-                            bundledTokens.push(token.lenient);
-                        }
+        vertex.forEach((token) => {
+            if (token.type != "typo" && token.auto != true) {
+                if (Object.hasOwn(token, "orig")) {
+                    if (userinput.startsWith(token.orig)) {
+                        //readVertex(token.to, userinput.slice(token.orig.length));
                     }
-                })
-                readVertex(vertices, vertex[0].to, graderTranslation + "[" + bundledTokens.join("/") + "]");
-                return
+                }
+                console.log(index + "+" + token.lenient + "+" + userinput);
+                if (token.lenient == " ") {
+                    readVertex(token.to, userinput);
+                } else if (userinput.startsWith(token.lenient)) {
+                    readVertex(token.to, userinput.slice(token.lenient.length));
+                } else {
+                    hintMessage = token.lenient;
+                }
             }
-        }
-        // or continue building translation without bundling, branching for each token
-        if (!shouldBundleTokens) {
-            vertex.forEach((token) => {
-                if (token.type != "typo" && token.auto != true) {
-                    if (Object.hasOwn(token, "orig")) {
-                        readVertex(vertices, token.to, graderTranslation + token.orig);
-                    } else {
-                        readVertex(vertices, token.to, graderTranslation + token.lenient);
-                    }
-                }
-            })
-        }
+        })
     }
 }
 
@@ -251,7 +268,9 @@ function sentenceToRegex(translationString) {
         .replaceAll(".", "\\.?")
         .replaceAll("!", "!?")
         .replaceAll(",", ",?")
+        .replaceAll("'", "'?")
         .replaceAll(" ", " ?")
         + "$"
     )
 }
+loadPrefetchedSessions();
